465f0a13c492c5f229f51747ba0f918b5a7d265a
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-11-19 22:04:14 +0200

    added navbar section and footer section

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer";
+import Navbar from "../pages/shared/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
